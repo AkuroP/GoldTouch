2278bc03bb5964d17ca02c432a6631dd744617fc
Ythome <timeoleb@gmail.com>
2024-11-07 13:42:34 +0100

    Ads interstitial
    al

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,9 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Advertisements;
 
-public class GameManager : MonoBehaviour
+
+public class GameManager : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener
+
 {
+    
+    // ADS
+    [SerializeField] string _androidAdUnitId = "Interstitial_Android";
+    [SerializeField] string _iOsAdUnitId = "Interstitial_iOS";
+    string _adUnitId;  
+    
+    
     [SerializeField]
     private GameObject[] _allGems;
 
@@ -74,9 +84,14 @@ public class GameManager : MonoBehaviour
         {
             instance = this;
         }
-        
+
+        //Ads
+        _adUnitId = (Application.platform == RuntimePlatform.IPhonePlayer)
+            ? _iOsAdUnitId
+            : _androidAdUnitId;
 
     }
+
     private void Start()
     {
         textScoreToBeat.text = "/ " + scoreToBeat;
@@ -85,7 +100,10 @@ public class GameManager : MonoBehaviour
         UpdateBonusTexts();
 
         //LoadBoolValues();
+        LoadAd(); 
     }
+
+
 
     public void ResetComboTimer() => _comboTimer = _comboMaxTimer;
 
@@ -116,8 +134,32 @@ public class GameManager : MonoBehaviour
                 PlayerPrefs.SetInt("Highscore", actualScore);
             }
 
+            int randomInt = UnityEngine.Random.Range(0, 10);
+
+            if (isEndless == false)
+            {
+                if (randomInt >= 0 && randomInt <= 2)
+                {
+                    ShowAd(); // Lance la pub si le nombre est compris entre 0 et 2
+                }
+
+                else
+                {
+                    EndGame();
+                }
+            }
+
+            else if (isEndless == true)
+            {
+                ShowAd(); // Lance la pub si endless
+            }
+
+
+
             
-            EndGame();
+
+            
+            
         }
         Debug.Log(SettingSystem.instance.donnees[0].starsPerLevel);
     }
@@ -462,6 +504,50 @@ public class GameManager : MonoBehaviour
     //        }
     //    }
     //}
+
+
+
+
+    //ADS
+
+        // Load content to the Ad Unit:
+    public void LoadAd()
+    {
+        // IMPORTANT! Only load content AFTER initialization (in this example, initialization is handled in a different script).
+        Debug.Log("Loading Ad: " + _adUnitId);
+        Advertisement.Load(_adUnitId, this);
+    }
+
+        // Show the loaded content in the Ad Unit:
+    public void ShowAd()
+    {
+        // Note that if the ad content wasn't previously loaded, this method will fail
+        Debug.Log("Showing Ad: " + _adUnitId);
+        Advertisement.Show(_adUnitId, this);
+    }
+
+        // Implement Load Listener and Show Listener interface methods: 
+    public void OnUnityAdsAdLoaded(string adUnitId)
+    {
+        // Optionally execute code if the Ad Unit successfully loads content.
+    }
+ 
+    public void OnUnityAdsFailedToLoad(string _adUnitId, UnityAdsLoadError error, string message)
+    {
+        Debug.Log($"Error loading Ad Unit: {_adUnitId} - {error.ToString()} - {message}");
+        // Optionally execute code if the Ad Unit fails to load, such as attempting to try again.
+    }
+ 
+    public void OnUnityAdsShowFailure(string _adUnitId, UnityAdsShowError error, string message)
+    {
+        Debug.Log($"Error showing Ad Unit {_adUnitId}: {error.ToString()} - {message}");
+        // Optionally execute code if the Ad Unit fails to show, such as loading another ad.
+    }
+ 
+    public void OnUnityAdsShowStart(string _adUnitId) { }
+    public void OnUnityAdsShowClick(string _adUnitId) { }
+    public void OnUnityAdsShowComplete(string _adUnitId, UnityAdsShowCompletionState showCompletionState) { EndGame();}
+
 
 
 }
